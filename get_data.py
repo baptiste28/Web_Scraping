b456c6f78ca6e15e1d4be8aa4dbe3f790256eb65
iva.doseva@bts.tech <iivkkaA98@>
2021-09-23 13:44:15 +0200

    Clear file with comments

--- get_data.py
+++ get_data.py
@@ -1,6 +1,5 @@
 from bs4 import BeautifulSoup
 import requests
-import urllib.request
 import os
 import pandas
 import csv
@@ -19,10 +18,12 @@ def html_code_stored_in_a_file(url):
 
 url = 'https://www.basketball-reference.com/leagues/NBA_2021_standings.html'
 
-#if the file exists do nothing
+#If the file exists do nothing
 if os.path.exists('./html_code.html'):
     pass
-#if the file does not exist, call the function html_code_stored_in_a_file
+
+#If the file does not exist, call the function html_code_stored_in_a_file. 
+#Keep in mind that we have to uncomment mannualy some part of the HTML code and save the file!!!!!
 else:
     html_code_stored_in_a_file(url)
 
@@ -41,51 +42,41 @@ with open('html_code.html') as html_file:
     #upper_headings = table.thead.find('tr').text
     #print(upper_headings)
 
+    #find all headings
     for i in table.thead.find('tr', class_=False):
         title=i.text.strip()
         headers.append(title)
 
+    # remove the Rk heading
     headers2=headers[2:]    
     print(headers2)
+    #remove empty elements from the list of headings
     new_headers = list(filter(None, headers2))
     print(new_headers)
 
+    #open the csv file
     csv_file = open('nba.csv', 'w')
+    #create a writer
     csv_writer = csv.writer(csv_file)
+    #write the headings to the csv file
     csv_writer.writerow(new_headers)
-    pprint.pprint(len(new_headers))
-
-    
-
-    data_frame=pandas.DataFrame(columns=new_headers)
 
     rows = []
+    #get all the data from the table rows
     for tr in table.tbody.findAll('tr'):
        for td in tr.findAll('td'):       
                value = td.text
                rows.append(value)
 
- 
+    #split the data into 30 lists because we have 30 teams
     spli_team = np.array_split(rows,30)
     for array in spli_team:
         b = (list(array))
+
+        #add the rows to the csv file
         csv_writer.writerow(b)
-        
 
-
-
-
-
-    # #for tr in table.tbody.find('tr'):
-    # #     for td in tr.findAll('td'):       
-    # #         value = td.text
-    # #         rows.append(value)
-    # #     print(rows)
-    # #         #data_frame.append(rows)
-    # # #print(data_frame)
-
-    csv_writer.writerow(rows)
-
+#close the file
 csv_file.close()
 
 
