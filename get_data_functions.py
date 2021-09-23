19700df2f0191c31f72b4a6b2b546602b6dc55c5
dosevaiva <90329505+dosevaiva@users.noreply.github.com>
2021-09-23 16:40:07 +0200

    Remove create_csv function

--- get_data_functions.py
+++ get_data_functions.py
@@ -57,20 +57,6 @@ def find_rows(table):
         #print(b)
     return spli_team 
 
-
-def create_csv(row):
-    #open the csv file
-        csv_file = open('nba_test.csv', 'w')
-        #create a writer
-        csv_writer = csv.writer(csv_file)
-        #write the headings to the csv file
-        csv_writer.writerow(headers)
-        #add the rows to the csv file
-        csv_writer.writerow(row)
-        #close the file
-        csv_file.close()
-
-
 url = 'https://www.basketball-reference.com/leagues/NBA_2021_standings.html'
 html_code_stored_in_a_file(url)
 
