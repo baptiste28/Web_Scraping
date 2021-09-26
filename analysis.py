4549cb0713cd7f8dc27a5e73e570a1fc34c1bae0
dosevaiva <90329505+dosevaiva@users.noreply.github.com>
2021-09-26 11:41:46 +0200

    Add a graph

--- analysis.py
+++ analysis.py
@@ -1,4 +1,5 @@
 import pandas as pd
+import matplotlib.pyplot as plt
 
 #get the data from the csv file
 data = pd.read_csv("nba_test.csv")
@@ -7,6 +8,7 @@ data = pd.read_csv("nba_test.csv")
 
 #define a class Team, which takes team name and performes the analysis
 class Team():
+
     def __init__(self, team_name):
         self.team_name = team_name
 
@@ -18,7 +20,20 @@ class Team():
         loses = int(overall.split('-')[1])
         win_loss_ratio = round((wins / (wins+loses) *100),2)
         print(f'The overall win/loss ratio of {self.team_name} is {win_loss_ratio}%!')
-        return win_loss_ratio
+
+        labels = 'Wins', 'Losses'
+        sizes = [wins,loses]
+        explode = (0.1 ,0)
+        colors = ['#e38d8d', '#95b7ed']
+        title = 'Oveall'
+
+        fig1, ax1 = plt.subplots()
+        ax1.pie(sizes, explode=explode, labels=labels, autopct='%1.1f%%',startangle=90, colors=colors)
+        ax1.axis('equal')  # Equal aspect ratio ensures that pie is drawn as a circle.
+        ax1.set_title('Overall')
+
+        plt.show() 
+        plt.close(1)       
 
 #This is to test and should be in main 
 team_name = input("Team name: ")
